a34da9cfd29f86fc83f4a5c8651413ae51bbf5d7
Charlie <charlie@charlies-mbp.home>
2022-12-07 21:34:11 +0000

    initial commit

---
+++ day-5/supply_stacks.py
@@ -0,0 +1,27 @@
+# Part 1
+# After the rearrangement procedure completes, what crate ends up on top of each stack
+
+with open("input.txt") as f:
+    input_list = f.readlines()
+
+crate_dict = dict()
+
+for row in input_list:
+    row = [ row[idx:idx + 4].strip() for idx in range(0, len(row), 4) ]
+    if row[0] != '1':
+        for i in range(1, 10):
+            crate_dict[i] = crate_dict.get(i, [])
+            if row[i-1] != '':
+                crate_dict[i].insert(0, row[i-1])
+    else:
+        break
+
+for command in input_list[10:]:
+    _, num_crates, _, from_stack, _, to_stack = command.strip().split(' ')
+    for idx in range(int(num_crates)):
+        crate_dict[int(to_stack)].append(crate_dict[int(from_stack)].pop())
+
+result = [crate_dict[key][-1] for key in crate_dict.keys()]
+result = [res.replace("[", "").replace("]", "") for res in result]
+
+print("".join(result))
